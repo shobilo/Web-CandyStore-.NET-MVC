7e74a3c49b599024065a49eb50ce18ef2ab7e27b
shobilo <58330458+shobilo@users.noreply.github.com>
2021-04-25 19:30:26 +0300

    [feat] Candy controller was added
    and a lot of auto trash dll

---
+++ CandyStore/UI/Controllers/CandyController.cs
@@ -0,0 +1,24 @@
+﻿using Domain.Abstract;
+using Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace UI.Controllers
+{
+    public class CandyController : Controller
+    {
+        // GET: Candy
+        private ICandyRepository repository;
+        public CandyController(ICandyRepository rep)
+        {
+            repository = rep;
+        }
+        public ViewResult List()
+        {
+            return View(repository.Candies);
+        }
+    }
+}
