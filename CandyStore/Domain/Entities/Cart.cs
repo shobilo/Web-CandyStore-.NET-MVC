0c0c7d19618b1e30560033ee112ceb7236644495
shobilo <58330458+shobilo@users.noreply.github.com>
2021-05-05 21:33:15 +0300

    [feat] Cart entity was added

---
+++ CandyStore/Domain/Entities/Cart.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entities
+{
+    public class Cart
+    {
+        private List<CartLine> lineCollection = new List<CartLine>();
+
+        public void AddItem(Candy candy, int quantity)
+        {
+            CartLine line = lineCollection
+                .Where(g => g.Candy.CandyId == candy.CandyId)
+                .FirstOrDefault();
+
+            if (line == null)
+            {
+                lineCollection.Add(new CartLine
+                {
+                    Candy = candy,
+                    Quantity = quantity
+                });
+            }
+            else
+            {
+                line.Quantity += quantity;
+            }
+        }
+
+        public void RemoveLine(Candy candy)
+        {
+            lineCollection.RemoveAll(l => l.Candy.CandyId == candy.CandyId);
+        }
+
+        public decimal ComputeTotalValue()
+        {
+            return lineCollection.Sum(e => e.Candy.Price * e.Quantity);
+
+        }
+        public void Clear()
+        {
+            lineCollection.Clear();
+        }
+
+        public IEnumerable<CartLine> Lines
+        {
+            get { return lineCollection; }
+        }
+    }
+
+    public class CartLine
+    {
+        public Candy Candy { get; set; }
+        public int Quantity { get; set; }
+    }
+}
