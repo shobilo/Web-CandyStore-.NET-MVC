a2d81060aaa29197d3963a1cd807afb9869025ee
shobilo <58330458+shobilo@users.noreply.github.com>
2021-05-05 20:37:05 +0300

    [fix] Link route was fixed

--- CandyStore/UI/App_Start/RouteConfig.cs
+++ CandyStore/UI/App_Start/RouteConfig.cs
@@ -9,14 +9,54 @@ namespace UI
 {
     public class RouteConfig
     {
+        //public static void RegisterRoutes(RouteCollection routes)
+        //{
+        //    routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+        //    routes.MapRoute(
+        //        name: null, 
+        //        url: "",
+        //        defaults: new { controller = "Candy", action = "List", category = (string)null, page = 1}
+        //    );
+
+        //    routes.MapRoute(
+        //        name: null,
+        //        url: "Page{page}",
+        //        defaults: new { controller = "Candy", action = "List", category = (string)null },
+        //        constraints: new { page = @"\d+" }
+        //    );
+
+        //    routes.MapRoute(
+        //        name: null,
+        //        url : "{category}",
+        //        defaults: new { controller = "Candy", action = "List", page = 1 }
+        //    );
+
+        //    routes.MapRoute(
+        //        name: null,
+        //        url: "{category}/Page{page}",
+        //        defaults: new { controller = "Candy", action = "List" },
+        //        constraints: new { page = @"\d+" }
+        //    );
+
+        //    routes.MapRoute(
+        //        name: null, 
+        //        url: "{controller}/{action}"
+        //    );
+        //}
         public static void RegisterRoutes(RouteCollection routes)
         {
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
-            routes.MapRoute(
-                name: null, 
-                url: "",
-                defaults: new { controller = "Candy", action = "List", category = (string)null, page = 1}
+            routes.MapRoute(null,
+                "",
+                new
+                {
+                    controller = "Candy",
+                    action = "List",
+                    category = (string)null,
+                    page = 1
+                }
             );
 
             routes.MapRoute(
@@ -26,23 +66,18 @@ namespace UI
                 constraints: new { page = @"\d+" }
             );
 
-            routes.MapRoute(
-                name: null,
-                url : "{category}",
-                defaults: new { controller = "Candy", action = "List", page = 1 }
+            routes.MapRoute(null,
+                "{category}",
+                new { controller = "Candy", action = "List", page = 1 }
             );
 
-            routes.MapRoute(
-                name: null,
-                url: "{category}/Page{page}",
-                defaults: new { controller = "Candy", action = "List" },
-                constraints: new { page = @"\d+" }
+            routes.MapRoute(null,
+                "{category}/Page{page}",
+                new { controller = "Candy", action = "List" },
+                new { page = @"\d+" }
             );
 
-            routes.MapRoute(
-                name: null, 
-                url: "{controller}/{action}"
-            );
+            routes.MapRoute(null, "{controller}/{action}");
         }
     }
 }
